e36a9c9ea8df987be06a12a960805f3f4ccbbfab
trxsh1 <yagosousa630@gmail.com>
2023-03-15 20:45:49 -0300

    remove page _404 of example

--- src/data/posts/count-all-posts.ts
+++ src/data/posts/count-all-posts.ts
@@ -1,5 +1,4 @@
 import { API_URL } from '@/config/app-config';
-import { fetchJson } from '@/utils/fetch-json';
 
 export const countAllPosts = async (query = ''): Promise<string> => {
   const url = `${API_URL}/count?${query}`;
--- src/pages/_404.tsx
+++
@@ -1,8 +0,0 @@
-// Page 404 opcional (Custom)
-// export default function PageNotFound() {
-//   return 'Page not found';
-// }
-
-export function a() {
-  return 1;
-}
