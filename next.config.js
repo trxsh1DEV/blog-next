5a6ff89459590125d056403164e898f2f9407db8
trxsh1 <yagosousa630@gmail.com>
2023-03-16 12:01:06 -0300

    t

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// eslint-disable-next-line no-undef
+module.exports = {
+  // Target must be serverless
+  target: 'serverless',
 };
-
-module.exports = nextConfig;
