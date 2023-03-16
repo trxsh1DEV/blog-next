1868f729e09ec8f3efa375225853b40ff910c6af
trxsh1 <yagosousa630@gmail.com>
2023-03-16 12:30:21 -0300

    att packages npm netlify/next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-undef */
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// module.exports = nextConfig;
-module.exports = {
-  target: 'serverless',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
